e11c85801fa0dc5d3d6995b7b61cc6286614f777
brettknadle <brett182x@gmail.com>
2023-09-22 17:41:46 -0700

    Course through stripe payments

---
+++ apps/payments/src/payments.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { stripVTControlCharacters } from 'util';
+import Stripe from 'stripe';
+import { ConfigService } from '@nestjs/config';
+import { CreateChargeDto } from '../../../libs/common/src/dto/create-charge.dto';
+
+@Injectable()
+export class PaymentsService {
+  private readonly stripe = new Stripe(
+    this.configService.get('STRIPE_SECRET_KEY'),
+    { apiVersion: '2023-08-16' },
+  );
+
+  constructor(private readonly configService: ConfigService) {}
+
+  async createCharge({ amount }: CreateChargeDto) {
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      payment_method: 'pm_card_visa',
+      amount: amount * 100,
+      confirm: true,
+      payment_method_types: ['card'],
+      currency: 'usd',
+    });
+
+    return paymentIntent;
+  }
+}
